f722f074d188937b99749b4f70940df84c34cb99
Damien <damienmarble@gmail.com>
2016-03-27 15:56:53 -0700

    Added drag-and-drop support to assigning characteristic values and added modal confirmation if trying to navigate away without assigning a value to all characteristics.

--- src/main/resources/js/characteristics.js
+++ src/main/resources/js/characteristics.js
@@ -1,23 +1,65 @@
-var roll = function(diceCount, dieMin, dieMax, modifier){
-	var result = modifier;
-	for(var i = 0; i < diceCount; i++){
-		result += Math.floor(Math.random() * (dieMax - dieMin+ 1)) + 1;
-	}
-	return result;
+var roll = function(diceCount, dieMin, dieMax, modifier) {
+    var result = modifier;
+    for (var i = 0; i < diceCount; i++) {
+        result += Math.floor(Math.random() * (dieMax - dieMin + 1)) + 1;
+    }
+    return result;
 }
 
-angular.module("OnlyWar").controller("CharacteristicsController", function($scope, characteristics, character) {
-    var characteristics = characteristics.query();
-    $scope.characteristics = characteristics;
+angular.module("OnlyWar").controller("CharacteristicsController", function($scope, characteristics, character, $uibModal, $state) {
+    var characteristicNames;
+    characteristics.query().$promise.then(function(result) {
+        characteristicNames = result;
+        $scope.characteristicNames = characteristicNames;
+
+    });
     $scope.character = character.character;
     $scope.generatedValues = [];
+    $scope.characteristicValues = character.character.characteristics;
     $scope.generate = function(index) {
-        if (index === undefined){
-        	for(var i = 0; i < characteristics.length; i++){
-        		$scope.generatedValues[i] = roll(2,1,10,20);
-        	}
+        if (index === undefined) {
+            for (var i = 0; i < characteristicNames.length; i++) {
+                $scope.generatedValues[i] = roll(2, 1, 10, 20);
+                $scope.characteristicValues[characteristicNames[i].toLowerCase()] = null;
+            }
         } else {
-        	$scope.generatedValues[index] = roll(2,1,10,20);
+            $scope.generatedValues[index] = roll(2, 1, 10, 20);
         }
     };
+
+    $scope.valueButtonClick = function(index) {
+        $scope.generate(index);
+    }
+
+    $scope.onDrop = function(result) {
+        dirty = true;
+    }
+    var suppressDialog = false;
+    var valuesAreDirty = function() {
+        var numValuesAssigned = 0;
+        for (var characteristic in $scope.characteristicValues) {
+            if ($scope.characteristicValues[characteristic] !== null) {
+                numValuesAssigned++;
+            };
+        };
+        return numValuesAssigned !== characteristicNames.length
+    };
+
+    $scope.$on('$stateChangeStart', function(e, toState, toParam, fromState, fromParams) {
+        if (fromState.name === "characteristics" && toState.name !== fromState.name && valuesAreDirty()) {
+            var resultHandler = function(result) {
+                if (result) {
+                    suppressDialog = true;
+                    $state.go(toState);
+                }
+            };
+            if (!suppressDialog) {
+                e.preventDefault();
+                confirm = $uibModal.open({
+                    controller: "ConfirmationController",
+                    templateUrl: "templates/confirm-navigation-modal.html"
+                }).result.then(resultHandler);
+            }
+        }
+    });
 });
