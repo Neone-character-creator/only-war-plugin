e56132a31f9a5c9c974e74757aada24e144ade45
Damien <damienmarble@gmail.com>
2016-03-28 12:36:28 -0700

    Added a dice service.

---
+++ src/main/resources/js/dice.js
@@ -0,0 +1,10 @@
+angular.module("OnlyWar").factory("dice", function(){
+	return {
+    	roll : function(diceCount, dieMin, dieMax, modifier) {
+        	var result = modifier | 0;
+        	for (var i = 0; i < diceCount; i++) {
+    	        result += Math.floor(Math.random() * (dieMax - dieMin + 1)) + 1;
+    	    }
+    	    return result;
+        }
+}})
