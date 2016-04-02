e3cad8f9875a2bb3d89277cef9965bb64b5e42cd
Damien <damienmarble@gmail.com>
2016-04-01 17:01:43 -0700

    Reorganized dice service.

--- src/main/resources/js/app/services/dice.js
+++ src/main/resources/js/app/services/dice.js
@@ -1,13 +1,15 @@
-define([], "dice", function() {
+define(function() {
     return function() {
+    	var roll = function(dieMin, dieMax, rollCount) {
+    		var result = 0;
+    		var rollCount = 1 | rollCount;
+    		for (var i = 0; i < rollCount; i++) {
+    			result += Math.floor(Math.random() * (dieMax - dieMin + 1)) + 1;
+    		};
+    		return result;
+    	};
         return {
-            roll: function(diceCount, dieMin, dieMax, modifier) {
-                var result = modifier | 0;
-                for (var i = 0; i < diceCount; i++) {
-                    result += Math.floor(Math.random() * (dieMax - dieMin + 1)) + 1;
-                }
-                return $q.then(result);
-            }
-        }
-	}
+            roll: roll
+        };
+	};
 });
