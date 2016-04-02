31878fc9b0bdebad8602b7a5fcd3cccca9ffd5aa
Damien <damienmarble@gmail.com>
2016-04-01 17:25:04 -0700

    Reorganized regiment selection page and controller.

--- src/main/resources/js/app/services/regiments.js
+++ src/main/resources/js/app/services/regiments.js
@@ -1,29 +1,29 @@
 define(function(){
-	return function($resource){
+	return function($resource, $q){
 		var regiments = $resource("Regiment/regiments.json").query();
 		var regimentNameToIndex = {};
 		var _selected = null;
-		var _selectionsRemaining;
-		var _selectedModified = false;
+		var _remainingSelections;
+		var _selectionComplete = false;
 
 		var service = {
             regiments : function() {
-            	return regiments.$promise.then(function(response){
-            		return response.splice();
+            	return regiments.$promise.then(function(result){
+            		return result.slice();
             	});
             },
             selected : function() {
             	return _selected;
             },
-            selectionsRemaining : function(){
-            	return _selectionsRemaining;
+            remainingSelections : function(){
+            	return _remainingSelections;
             },
-            dirty : function(){
-            	return _selectedModified;
+            complete : function(){
+            	return _selected && _remainingSelections.length === 0;
             },
 	            selectRegiment : function(regiment) {
 	                _selected = regiment;
-	                _selectionsRemaining = _selected['optional modifiers'];
+	                _remainingSelections = _selected['optional modifiers'];
 	            }
 	        };
 		return service;
