5ae7cd46f21d828a68b5a7efb1cc7d798c2313b6
Damien <damienmarble@gmail.com>
2016-04-06 16:52:59 -0700

    Added a Prerequisite type that represents talent or psychic power prerequisites, a function that transforms powers and talents as they are loaded, inserting prerequisite objects, changed the talents and powers to be transformed the first time they are loaded and implemented getting psychic powers.

--- src/main/resources/js/app/services/characteroptions.js
+++ src/main/resources/js/app/services/characteroptions.js
@@ -1,15 +1,62 @@
 define(function(){
-	return function($resource){
+	function Prerequisite(property, name, value){
+			var _property = property;
+			var _name = name;
+			var _value = value;
+			var prerequisite = {
+				property : property,
+				name : name,
+				value : value,
+				check : function(character){
+					var testValue;
+					switch(_property){
+						case "characteristic":
+							switch(value.comparison){
+								case "at least" :
+									return character.characteristics().byName(_name.toLowerCase()) >= value.value;
+							};
+						break;
+						case "power":
+							return character.powers().all().filter(function(element){
+								return element.name === _value;
+							}).length > 0;
+						break;
+						case "skill":
+							return character.skills().byName(_name);
+						break;
+						case "talent":
+							return character.talents().byName(_name);
+						break;
+					};
+				}
+			};
+			Object.freeze(prerequisite);
+			return prerequisite;
+		};
+	function transform(element){
+		if(element.prerequisites){
+			element.prerequisites = element.prerequisites.map(function(element){
+				return new Prerequisite(element.property, element.name, element.value);
+			});
+		};
+		return element;
+	};
+	return function($resource, $q){
 		var characteristics = $resource("Character/characteristics.json").query();
-		var talents = $resource("Character/Talents.json").query();
+		var talents = $resource("Character/Talents.json").query().$promise.then(function(result){
+			return $q.resolve(result.map(transform));
+		});
 		var skills = $resource("Character/Skills.json").query();
-		var powers = $resource("Character/Psychic Powers.json").query();
+		var powers = $resource("Character/Psychic Powers.json").query().$promise.then(function(result){
+			return $q.resolve(result.map(transform));
+		});
 		var fatePointRolls = $resource("Character/fatepoints.json").get();
 		var xpCosts = $resource("Character/advancementcosts.json").get();
 
 		return {
 			talents: function(){
-				return talents.$promise.then(function(result){
+			if (talents.$promise.resolved)
+				return talents.then(function(result){
 					return result.slice();
 				});
 			},
@@ -19,7 +66,9 @@ define(function(){
 				});
 			},
 			powers : function(){
-				throw "Not implemented.";
+				return powers.then(function(result){
+					return result.slice();
+				});
 			},
 			characteristics : function(){
 				return characteristics.$promise.then(function(result){
