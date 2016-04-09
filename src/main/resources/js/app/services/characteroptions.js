cc494b119f4c48db907362553599bdbf2c2848a8
Damien <damienmarble@gmail.com>
2016-04-09 01:10:22 -0700

    Implemented placeholder replacement in characteroptions.js. Not finished.

--- src/main/resources/js/app/services/characteroptions.js
+++ src/main/resources/js/app/services/characteroptions.js
@@ -5,28 +5,28 @@ properties of the character that will be traversed.
 Value represents the comparison to make to determine if the tested character meets the prerequisite.
 */
 function Prerequisite(properties, value) {
-		var _properties = properties;
-		var _value = value;
-		var prerequisite = {
-			properties: _properties,
-			value: value,
-			check: function(character) {
-				var testValue = character;
-				$.each(_properties, function(index, property){
-					testValue = testValue[property];
-				});
-				switch(value.comparison){
-					case "at least":
-					 	return testValue >= _value;
+	var _properties = properties;
+	var _value = value;
+	var prerequisite = {
+		properties: _properties,
+		value: value,
+		check: function(character) {
+			var testValue = character;
+			$.each(_properties, function(index, property) {
+				testValue = testValue[property];
+			});
+			switch (value.comparison) {
+				case "at least":
+					return testValue >= _value;
 					break;
-					default:
-						return testValue === _value;
-				}
+				default:
+					return testValue === _value;
 			}
-		};
-		Object.freeze(prerequisite);
-		return prerequisite;
+		}
 	};
+	Object.freeze(prerequisite);
+	return prerequisite;
+};
 
 define(function() {
 	function createPrerequisites(element) {
@@ -39,6 +39,136 @@ define(function() {
 	};
 
 	return function($resource, $q) {
+		/** Goes through the given regiment or specialty modifier and replaces all of the placeholder skill, talent and
+    	equipment names with their actual object versions.
+    	*/
+		function transformPlaceholders(modifier) {
+			var fixedModifiers = modifier['fixed modifiers'];
+			var optionalModifiers = modifier['optional modifiers'];
+			var modifierSkills = $q.defer();
+			//Fixed Modifiers
+			skills.$promise.then(function(result) {
+				var replacementSkills = {};
+				for (skill in fixedModifiers.skills) {
+					var specialization = skill.indexOf("(") < 0 ? null : skill.substring(skill.indexOf("(") + 1, skill.indexOf(")"));
+					var baseName = skill.substring(0, skill.indexOf("(") < 0 ? skill.length : skill.indexOf("(")).trim();
+					replacementSkills[skill] = result.filter(function(element) {
+						return element.name === baseName;
+					})[0];
+					if (specialization) {
+						replacementSkills[skill].specialization = specialization;
+					}
+				};
+				modifierSkills.resolve(replacementSkills);
+			});
+			var modifierTalents = $q.defer();
+			talents.then(function(result) {
+				var replacementTalents = fixedModifiers.talents;
+				if (replacementTalents) {
+					replacementTalents = replacementTalents.slice();
+					replacementTalents = replacementTalents.map(function(element) {
+						var specialization = element.indexOf("(") < 0 ? null : element.substring(element.indexOf("(") + 1, element.indexOf(")"));
+						element = element.substring(0, specialization ? element.indexOf("(") : element.length).trim();
+						element = result.filter(function(talent) {
+							return element === talent.name;
+						})[0];
+						if (specialization) {
+							result.name += " (" + specialization + ")";
+						}
+						return element;
+					});
+				}
+				modifierTalents.resolve(replacementTalents);
+			});
+			var modifierEquipment = $q.defer();
+			function replace(name, source) {
+				return source.filter(function(element) {
+					return element.name === name;
+				})[0];
+			};
+			$q.all([weapons, armor, items, vehicles]).then(function(results) {
+				var characterKit = fixedModifiers['character kit'];
+				var replacementMainWeapons;
+				var replacementOtherWeapons;
+				var replacementArmor;
+				var replacementOtherItems;
+				if (characterKit) {
+					if (characterKit['main weapon']) {
+						replacementMainWeapons = characterKit['main weapon'].slice().map(function(weapon) {
+							weapon.item = replace(weapon.item.name, results[0]);
+							return weapon;
+						});
+					}
+					if (characterKit['other weapon']) {
+						replacementOtherWeapons = characterKit['other weapons'].slice().map(function(weapon) {
+							weapon.item = replace(weapon.item.name, results[0]);
+							return weapon;
+						});
+					}
+					if (characterKit['armor']) {
+						 replacementArmor = characterKit['armor'].slice();
+						replacementArmor = replacementArmor.map(function(armor) {
+							armor.item = replace(armor.item.name, results[1]);
+							return armor;
+						});
+					}
+					if (characterKit['other gear']) {
+						replacementOtherItems = characterKit['other gear'].slice();
+						replacementOtherItems = replacementOtherItems.map(function(item) {
+							item.item = replace(item.item.name, results[2].concat(results[3]));
+							return item;
+						});
+					}
+					modifierEquipment.resolve({
+						'main weapon': replacementMainWeapons,
+						'other weapons': replacementOtherWeapons,
+						'armor': replacementArmor,
+						'other gear': replacementOtherItems
+					});
+				} else {
+					modifierEquipment.resolve(undefined);
+				}
+			});
+			//Optional Modifiers
+			$q.all([talents,skills,weapons,armor,items]).then(function(result){
+				$.each(optionalModifiers, function(index, element){
+					$.each(element.options, function(index, option){
+						switch(option.property){
+							case "talents":
+							var name = option.value;
+							option.value = result[0].filter(function(element){
+								return element.name === option.value;
+							})[0];
+							if(!option.value){
+								throw "Tried to replace talent " + name + " in " + modifier.name + " but no talent by that name was found."
+							}
+							break;
+							case "skills":
+							break;
+						}
+					});
+				});
+			});
+
+			return $q.all([modifierSkills, modifierTalents, modifierEquipment]).then(function(results) {
+				results[0].promise.then(function(result) {
+					if(result){
+						fixedModifiers['skills'] = result;
+					}
+				});
+				results[1].promise.then(function(result) {
+					if(result){
+						fixedModifiers['talents'] = result;
+					}
+				});
+				results[2].promise.then(function(result) {
+					if(result){
+						fixedModifiers['character kit'] = result;
+					}
+				});
+				return modifier;
+			});
+		};
 		var characteristics = $resource("Character/characteristics.json").query();
 		var talents = $resource("Character/Talents.json").query().$promise.then(function(result) {
 			return $q.resolve(result.map(createPrerequisites));
@@ -53,77 +183,12 @@ define(function() {
 		var armor = $resource("Character/Armor.json").query();
 		var items = $resource("Character/Items.json").query();
 		var specialties = $resource("Character/Specialties.json").query().$promise.then(function(result) {
-			function convertEquipment(items, type) {
-        					return items.map(function(element) {
-        						var item;
-        						switch (type) {
-        							case "weapon":
-        								item = weapons.filter(function(weapon) {
-        									return element.item.name === weapon.name;
-        								})[0];
-        								break;
-        							case "armor":
-        								item = armor.filter(function(weapon) {
-        									return element.item.name === weapon.name;
-        								})[0];
-        								break;
-        							case "item":
-        								item = items.filter(function(weapon) {
-        									return element.item.name === weapon.name;
-        								})[0];
-        								break;
-        						}
-        						if (!item) {
-        							console.log("Tried to load item " + element.item.name + " but it wasn't found!");
-        							item = element;
-        						}
-        						item.craftsmanship = element.item.craftsmanship ? element.item.craftsmanship : "Common";
-        						item.upgrades = element.item.upgrades ? element.item.upgraded : [];
-        						return item;
-        					});
-        				};
-        	for (var i = 0; i < result.length; i++) {
-        					var equipment = result[i]['fixed modifiers']['character kit'];
-        					if (equipment) {
-        						for (var category in equipment) {
-        							var type;
-        							switch (category) {
-        								case "main weapon":
-        								case "other weapons":
-        									type = "weapon";
-        									break;
-        								case "other gear":
-        									type = "item";
-        									break;
-        								case "armor":
-        									type = "armor";
-        							}
-        							result[i]['fixed modifiers']['character kit'][category] = convertEquipment(equipment[category], type);
-        						}
-        					};
-        					$.each(result[i]['optional modifiers'], function(index, selection) {
-        						$.each(selection.options, function(index, option) {
-        							if (Array.isArray(option.property) && option.property[0] === "character kit") {
-        								var type;
-        								switch (element.options[o].property[1]) {
-        									case "main weapon":
-        									case "other weapons":
-        										type = "weapon";
-        										break;
-        									case "other gear":
-        										type = "item";
-        										break;
-        									case "armor":
-        										type = "armor";
-        								}
-        								option.value = con;
-        							}
-        						})
-        					});
-        				};
-        	return result;
-        });
-        var regiments = $resource("Regiment/regiments.json").query();
+			return $q.all(result.map(transformPlaceholders));
+		});
+		var regiments = $resource("Regiment/regiments.json").query().$promise.then(function(result) {
+			return $q.all(result.map(transformPlaceholders));
+		});
+		var vehicles = $resource("Character/Vehicles.json").query();
 
 		return {
 			talents: function() {
@@ -162,22 +227,27 @@ define(function() {
 				})
 			},
 			weapons: function() {
-				return weapons.$promise.then(function(result){
+				return weapons.$promise.then(function(result) {
 					return result.slice();
 				});
 			},
 			armor: function() {
-				return armor.$promise.then(function(result){
+				return armor.$promise.then(function(result) {
 					return result.slice();
 				});
 			},
 			items: function() {
-				return items.$promise.then(function(result){
+				return items.$promise.then(function(result) {
 					return result.slice();
 				});
 			},
-			regiments : function(){
-				return regiments.$promise.then(function(result){
+			regiments: function() {
+				return regiments.then(function(result) {
+					return result.slice();
+				});
+			},
+			specialties: function() {
+				return specialties.then(function(result) {
 					return result.slice();
 				});
 			}
