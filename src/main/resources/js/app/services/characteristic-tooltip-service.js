0c66ddc0d75f82288f818c48492643ad0e9cb05a
Damien <damienmarble@gmail.com>
2016-04-10 22:47:38 -0700

    Changed to use getters and setters  in characteristic tooltip service.

--- src/main/resources/js/app/services/characteristic-tooltip-service.js
+++ src/main/resources/js/app/services/characteristic-tooltip-service.js
@@ -1,14 +1,9 @@
 define(function(){
 	return function(){
-	var _displayed = null;
-	return {
-		displayed : function(value){
-			if (value){
-				_displayed = value;
-			} else {
-				return _displayed;
-			}
-		}
-	};
+		var _displayed = null;
+		return {
+			get displayed(){return _displayed;},
+			set displayed(displayed){_displayed = displayed;}
+		};
 	}
 })
