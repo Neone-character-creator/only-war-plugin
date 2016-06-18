18f1e93b1be7fb56179a40825878f8360f0fbf03
Damien <damienmarble@gmail.com>
2016-06-18 13:37:39 -0700

    Added some comments to the selection service.

--- src/main/resources/pluginresource/js/app/services/selection.js
+++ src/main/resources/pluginresource/js/app/services/selection.js
@@ -1,3 +1,11 @@
+/**
+Service for decomposing a collection of possible values into a collection of actual values.
+
+This service was designed for situations where users are given a number of options to choose from.
+
+This service provides no sanity checking; it is the responsibility of the caller to ensure that the selection object
+remains unchanged through to completion and that the indices given to the choose function are valid.
+*/
 define(function() {
 	return function(){
 		return {
@@ -5,7 +13,7 @@ define(function() {
 			selectionObject: null,
 			//The most recently chosen values
 			selected: [],
-			//Decompose this option if valid selections made
+			//Decompose this option if valid selections made.
 			choose: function(selectedIndices) {
 				var selectionObject = this.selectionObject;
 				if (selectedIndices.length !== selectionObject.selections) {
