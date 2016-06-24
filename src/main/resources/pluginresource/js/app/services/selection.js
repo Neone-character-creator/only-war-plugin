d27c3a527e2e1dc63fdb8a73bebbddae1f4af5b6
Damien <damienmarble@gmail.com>
2016-06-24 14:15:50 -0700

    Changed the selection service to add validation when setting the selectionObject.

--- src/main/resources/pluginresource/js/app/services/selection.js
+++ src/main/resources/pluginresource/js/app/services/selection.js
@@ -8,9 +8,25 @@ remains unchanged through to completion and that the indices given to the choose
 */
 define(function() {
 	return function(){
+		var _selectionObject = null;
 		return {
 			//The selection object being chosen from
-			selectionObject: null,
+			set selectionObject(value){
+				if(typeof value.selections !== "number"){
+					throw "Selection object selections value must be a number but was " + typeof value.selections;
+				}
+				if(!Array.isArray(value.options)){
+					throw "Selection object options value must be an array.";
+				}
+				if(value.selections < 1){
+					throw "Selection object needs to require at least 1 selection.";
+				}
+				if(value.selections > value.options.length){
+					throw "Selection object can't require more selections than the number of options it has."
+				}
+				_selectionObject = value;
+			},
+			get selectionObject(){return _selectionObject},
 			//The most recently chosen values
 			selected: [],
 			//Decompose this option if valid selections made.
