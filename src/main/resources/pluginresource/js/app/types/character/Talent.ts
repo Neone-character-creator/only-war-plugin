7d0c2fe761c80a4c2d553a845d161f3d9088d542
Damien <damienmarble@gmail.com>
2016-07-07 16:29:26 -0700

    Added fields for aptitudes and specialization. Added prerequisites field to match HasPrerequisite interface.

--- src/main/resources/pluginresource/js/app/types/character/Talent.ts
+++ src/main/resources/pluginresource/js/app/types/character/Talent.ts
@@ -4,12 +4,6 @@ import {OnlyWarCharacter} from "./Character";
  * Created by Damien on 6/28/2016.
  */
 export class Talent implements HasPrerequisites<OnlyWarCharacter> {
-    prerequisites:Prerequisites<OnlyWarCharacter>;
-
-    meetsPrerequisites(target:OnlyWarCharacter):boolean {
-        return this.prerequisites.match(target);
-    }
-
     /**
      * The name of the talent.
      */
@@ -22,4 +16,52 @@ export class Talent implements HasPrerequisites<OnlyWarCharacter> {
      * The tier of the talent.
      */
     private _tier:number;
+    private _specialization:string;
+    private _aptitudes:Array<string>;
+    prerequisites:Prerequisites<OnlyWarCharacter>;
+    /**
+     * The maximum number of times this talent can be
+     */
+    private _maxTimesPurchaseable;
+
+    meetsPrerequisites(target:OnlyWarCharacter):boolean {
+        return this.prerequisites.match(target);
+    }
+
+    get name():String {
+        return this._name;
+    }
+
+    get source():String {
+        return this._source;
+    }
+
+    get tier():number {
+        return this._tier;
+    }
+
+    get specialization():string {
+        return this._specialization;
+    }
+
+    get aptitudes():Array<string> {
+        return this._aptitudes;
+    }
+
+    get maxTimesPurchaseable() {
+        return this._maxTimesPurchaseable;
+    }
+
+    constructor(name:String, source:String, tier:number, specialization?:string, prerequisites?:Prerequisites<OnlyWarCharacter>, maxTimesPurchaseable?:number) {
+        this.prerequisites = prerequisites;
+        this._name = name;
+        this._source = source;
+        this._tier = tier
+        this._specialization = specialization;
+        if (maxTimesPurchaseable) {
+            this._maxTimesPurchaseable = maxTimesPurchaseable;
+        } else {
+            this._maxTimesPurchaseable = 1;
+        }
+    }
 }
