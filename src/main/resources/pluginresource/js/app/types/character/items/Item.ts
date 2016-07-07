26381d78fdd37e727716e0645fa33c9512dd52ac
Damien <damienmarble@gmail.com>
2016-07-07 00:32:41 -0700

    Added constructor to Item.

--- src/main/resources/pluginresource/js/app/types/character/items/Item.ts
+++ src/main/resources/pluginresource/js/app/types/character/items/Item.ts
@@ -6,6 +6,16 @@ export class Item {
     private _craftsmanship:ItemCraftsmanship;
     private _weight:Number;
 
+    constructor(name:String, craftsmanship:ItemCraftsmanship, weight?:Number) {
+        this._name = name;
+        this._craftsmanship = craftsmanship;
+        if (weight) {
+            this._weight = weight;
+        } else {
+            this._weight = 0;
+        }
+    }
+
     get name():String {
         return this._name;
     }
