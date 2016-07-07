e65386f198d62a215d68b3e4c4bd6ba7bacd73d4
Damien <damienmarble@gmail.com>
2016-07-07 16:34:30 -0700

    Added an applyWoundsModifier method and implementations in Specialty and Regiment.

--- src/main/resources/pluginresource/js/app/types/character/Regiment.ts
+++ src/main/resources/pluginresource/js/app/types/character/Regiment.ts
@@ -2,14 +2,15 @@ import {CharacterModifier, OnlyWarCharacterModifierTypes, SelectableModifier} fr
 import {Talent} from "./Talent";
 import {Item} from "./items/Item";
 import {Trait} from "./Trait";
-import {CharacteristicName} from "./Characteristic";
+import {Characteristic} from "./Characteristic";
 import {OnlyWarCharacter} from "./Character";
+import {Skill} from "./Skill";
 /**
  * A fully complete regiment modifier
  * Created by Damien on 6/27/2016.
  */
 export class Regiment extends CharacterModifier {
-    constructor(characteristics:Map<CharacteristicName, number>, skills:Map<[string,string], number>, talents:Array<Talent>, aptitudes:Array<string>, traits:Array<Trait>, kit:Array<Item>, wounds:number) {
+    constructor(characteristics:Map<Characteristic, number>, skills:Array<Skill>, talents:Array<Talent>, aptitudes:Array<string>, traits:Array<Trait>, kit:Array<Item>, wounds:number) {
         super(characteristics, skills, talents, aptitudes, traits, kit, wounds, 0, OnlyWarCharacterModifierTypes.REGIMENT);
     }
 
@@ -20,5 +21,9 @@ export class Regiment extends CharacterModifier {
         for (var characteristic of this._characteristics) {
             character.characteristics.get(characteristic[0]).regimentModifier = characteristic[1];
         }
+    }
+
+    protected applyWoundsModifier(character:OnlyWarCharacter) {
+        character.wounds.regimentModifier = this._wounds;
     }
 }
--- src/main/resources/pluginresource/js/app/types/character/Specialty.ts
+++ src/main/resources/pluginresource/js/app/types/character/Specialty.ts
@@ -2,12 +2,18 @@ import {CharacterModifier, OnlyWarCharacterModifierTypes} from "./CharacterModif
 import {Talent} from "./Talent";
 import {Trait} from "./Trait";
 import {Item} from "./items/Item";
-import {CharacteristicName} from "./Characteristic";
+import {Characteristic} from "./Characteristic";
+import {OnlyWarCharacter} from "./Character";
+import {Skill} from "./Skill";
 /**
  * Created by Damien on 6/29/2016.
  */
 export class Specialty extends CharacterModifier {
-    constructor(characteristics:Map<CharacteristicName, number>, skills:Map<[string,string], number>, talents:Array<Talent>, aptitudes:Array<string>, traits:Array<Trait>, kit:Array<Item>, wounds:number) {
+    constructor(characteristics:Map<Characteristic, number>, skills:Array<Skill>, talents:Array<Talent>, aptitudes:Array<string>, traits:Array<Trait>, kit:Array<Item>, wounds:number) {
         super(characteristics, skills, talents, aptitudes, traits, kit, wounds, 0, OnlyWarCharacterModifierTypes.SPECIALTY);
+    }
+
+    protected applyWoundsModifier(character:OnlyWarCharacter) {
+        character.wounds.specialtyModifier = this._wounds;
     }
 }
