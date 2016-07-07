331fd04c637b113d2e4986474afc0c1b2577413c
Damien <damienmarble@gmail.com>
2016-07-06 23:21:34 -0700

    Added a class for traits that can have numeric ratings.

---
+++ src/main/resources/pluginresource/Character/RatedTrait.ts
@@ -0,0 +1,18 @@
+import {Trait} from "../js/app/types/character/Trait";
+/**
+ * Created by Damien on 7/6/2016.
+ */
+export class RatedTrait extends Trait {
+
+    private _rating:number;
+
+    constructor(name:String, description:String, rating:number) {
+        super(name, description);
+        this._rating = rating;
+    }
+
+
+    get rating():number {
+        return this._rating;
+    }
+}
