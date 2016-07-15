138cf41dad1ba8e1a07971020733d41ccc1c59b8
Damien <damienmarble@gmail.com>
2016-07-15 01:43:53 -0700

    Added availability and type fields to Item.

--- src/main/resources/pluginresource/js/app/types/character/items/Item.ts
+++ src/main/resources/pluginresource/js/app/types/character/items/Item.ts
@@ -3,35 +3,61 @@
  */
 export class Item {
     private _name:String;
-    private _craftsmanship:ItemCraftsmanship;
+    private _craftsmanship:Craftsmanship;
     private _weight:Number;
+    private _availability:Availability;
+    private _type:ItemType;
 
-    constructor(name:String, craftsmanship:ItemCraftsmanship, weight?:Number) {
+    constructor(name:String, type:ItemType, availability:Availability, weight?:Number, craftsmanship?:Craftsmanship) {
         this._name = name;
         this._craftsmanship = craftsmanship;
+        this._availability = availability;
         if (weight) {
             this._weight = weight;
         } else {
             this._weight = 0;
         }
+        if (craftsmanship) {
+            this._craftsmanship = craftsmanship;
+        } else {
+            this._craftsmanship = Craftsmanship.Common;
+        }
+        this._type = type;
     }
 
     get name():String {
         return this._name;
     }
 
-    get craftsmanship():ItemCraftsmanship {
+    get craftsmanship():Craftsmanship {
         return this._craftsmanship;
     }
 
     get weight():Number {
         return this._weight;
     }
+
+
+    get availability():Availability {
+        return this._availability;
+    }
 }
 
-export enum ItemCraftsmanship{
-    POOR,
-    COMMON,
-    GOOD,
-    BEST
+export enum Craftsmanship{
+    Poor = 1,
+    Common,
+    Good,
+    Best
+}
+
+export enum ItemType{
+    Weapon,
+    Armor,
+    Other
+}
+
+export enum Availability{
+    Ubiquitous,
+    Abundant,
+    Plentiful
 }
