9317b4bfcf494de50e794de2be370075f364af61
Damien <damienmarble@gmail.com>
2016-07-15 02:17:45 -0700

    Updated specialty provider service.

--- src/main/resources/pluginresource/js/app/services/specialties.js
+++ src/main/resources/pluginresource/js/app/services/specialties.js
@@ -3,12 +3,14 @@
  */
 define(function () {
     return function ($resource, $q, placeholders) {
-        var specialties = $resource("pluginresource/Character/Specialties.json").query().$promise.then(function (result) {
-            return $q.all(result.map(placeholders.replace));
+        var specialties = $q.defer();
+        placeholders.then(function (placeholders) {
+            specialties.resolve($resource("pluginresource/Character/Specialties.json").query().$promise.then(function (result) {
+                return $q.all(result.map(placeholders.replace));
+            }));
         });
-
-        return specialties.then(function (result) {
+        return specialties.promise.then(function (result) {
             return result.splice();
-        })
+        });
     }
 });
