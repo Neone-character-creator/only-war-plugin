1ee38ba3660616fe87a0f23ff00acbb02d74e575
Damien <damienmarble@gmail.com>
2016-07-15 01:13:59 -0700

    Added getters for specialization and aptitudes. Changed some properties from String to string.

--- src/main/resources/pluginresource/js/app/types/character/Skill.ts
+++ src/main/resources/pluginresource/js/app/types/character/Skill.ts
@@ -5,7 +5,7 @@ export class Skill {
     /**
      * The name of the skill.
      */
-    private _name:String;
+    private _name:string;
     /**
      * The rank of the skill, from 0 to 4, mapped to the Only War skill ratings.
      *
@@ -19,23 +19,33 @@ export class Skill {
     /**
      * The specialization of the skill, if it has one.
      */
-    private _specialization:String;
+    private _specialization:string;
+    private _aptitudes:Array<string>;
 
-    constructor(name:String, rank:number, specialization:String = null) {
+    constructor(name:string, rank:number, aptitudes:Array<string>, specialization:string = null) {
         if (rank < 0 || rank > 4) {
-            throw "The rank must be between 0 and 4 but was" + rank + ".";
+            throw "The rank must be at least 0 and less than 4 but was" + rank + ".";
         }
         this._name = name;
         this._rank = rank;
+        this._aptitudes = aptitudes;
         this._specialization = specialization;
     }
 
-    get name():String {
+    get name():string {
         return this._name;
     }
 
     get rank():number {
         return this._rank;
+    }
+
+    get specialization():string {
+        return this._specialization;
+    }
+
+    get aptitudes():Array<string> {
+        return this._aptitudes;
     }
 
     set rank(value:number) {
@@ -44,6 +54,4 @@ export class Skill {
         }
         this._rank = value;
     }
-
-
 }
