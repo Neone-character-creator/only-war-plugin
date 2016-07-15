b8e063a9884d2cbeaa9929e32be6fbf98be55d16
Damien <damienmarble@gmail.com>
2016-07-15 02:08:43 -0700

    WTF.

--- src/main/resources/pluginresource/js/app/types/character/Trait.js
+++ src/main/resources/pluginresource/js/app/types/character/Trait.js
@@ -1,19 +1,23 @@
-/**
- * Represents a character trait.
- * Created by Damien on 6/29/2016.
- */
-export class Trait {
-    constructor(name, description) {
-        this._name = name;
-        this._description = description;
-    }
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    /**
+     * Represents a character trait.
+     * Created by Damien on 6/29/2016.
+     */
+    class Trait {
+        constructor(name, description) {
+            this._name = name;
+            this._description = description;
+        }
 
-    get name() {
-        return this._name;
-    }
+        get name() {
+            return this._name;
+        }
 
-    get description() {
-        return this._description;
+        get description() {
+            return this._description;
+        }
     }
-}
+    exports.Trait = Trait;
+});
 //# sourceMappingURL=Trait.js.map
