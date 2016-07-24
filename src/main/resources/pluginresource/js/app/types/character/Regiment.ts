13787e2e6abb77f22a2d4b1886872865613fb91e
Damien <damienmarble@gmail.com>
2016-07-24 01:46:55 -0700

    Changed Regiment to use new Skill modifier format.
    Implemented apply and unapply methods.

--- src/main/resources/pluginresource/js/app/types/character/Regiment.ts
+++ src/main/resources/pluginresource/js/app/types/character/Regiment.ts
@@ -4,13 +4,21 @@ import {Item} from "./items/Item";
 import {Trait} from "./Trait";
 import {Characteristic} from "./Characteristic";
 import {OnlyWarCharacter} from "./Character";
-import {Skill} from "./Skill";
+import {Skill, SkillDescription} from "./Skill";
 /**
  * A fully complete regiment modifier
  * Created by Damien on 6/27/2016.
  */
 export class Regiment extends CharacterModifier {
-    constructor(name:string, characteristics:Map<Characteristic, number>, skills:Array<Skill>, talents:Array<Talent>, aptitudes:Array<string>, traits:Array<Trait>, kit:Map<Item, number>, wounds:number, optionalModifiers:Array<SelectableModifier>) {
+    constructor(name:string,
+                characteristics:Map<Characteristic, number>,
+                skills:Map<SkillDescription, number>,
+                talents:Array<Talent>,
+                aptitudes:Array<string>,
+                traits:Array<Trait>,
+                kit:Map<Item, number>,
+                wounds:number,
+                optionalModifiers:Array<SelectableModifier>) {
         super(characteristics, skills, talents, aptitudes, traits, kit, wounds, 0, OnlyWarCharacterModifierTypes.REGIMENT);
         this._name = name;
         this._optionalModifiers = optionalModifiers;
@@ -18,14 +26,20 @@ export class Regiment extends CharacterModifier {
 
     private _optionalModifiers:Array<SelectableModifier>
 
-    protected applyCharacteristicModifiers(character:OnlyWarCharacter):any {
-        for (var characteristic of this.characteristics) {
-            character.characteristics.get(characteristic[0]).regimentModifier = characteristic[1];
+    public apply(character:OnlyWarCharacter):any {
+        super.apply(character);
+        for (var entry of this.characteristics.entries()) {
+            character.characteristics.get(entry[0]).regimentModifier = entry[1];
         }
+        character.wounds.regimentModifier = this.wounds;
     }
 
-    protected applyWoundsModifier(character:OnlyWarCharacter) {
-        character.wounds.regimentModifier = this.wounds;
+    public unapply() {
+        for (var entry of this.characteristics.entries()) {
+            this._appliedTo.characteristics.get(entry[0]).regimentModifier = 0;
+        }
+        this._appliedTo.wounds.regimentModifier = 0;
+        super.unapply();
     }
 
     private _name:string;
