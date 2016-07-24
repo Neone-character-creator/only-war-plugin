1a010903420c6c16156164331593d5ea754dd7f6
Damien <damienmarble@gmail.com>
2016-07-24 13:04:38 -0700

    Changed Prerequisite to use a predicate function instead of objects.

--- src/main/resources/pluginresource/js/app/types/Prerequisite.ts
+++ src/main/resources/pluginresource/js/app/types/Prerequisite.ts
@@ -7,7 +7,7 @@
  * The generic type parameter specifies the type that the prerequisite targets.
  */
 export class Prerequisite<T> {
-    private _matcher:Array<any>;
+    private _predicate:Function;
 
     /**
      * Tests this Prerequisite object against the given target object.
@@ -16,24 +16,12 @@ export class Prerequisite<T> {
      * @param target
      */
     public match(target:T):boolean {
-        return !this._matcher || this._matcher.map(predicate=> {
-                return predicate(target);
-            }).reduce((previous, current, currentIndex)=> {
-                if (current === undefined) {
-                    throw "The predicate at " + currentIndex + " in this Prerequisite returned undefined";
-                }
-                return previous && current;
-            }, true);
+        return this._predicate === undefined || this._predicate(target);
     }
 
-    constructor(matcher?:Array<any>) {
-        if (matcher) {
-            for (var i = 0; i < matcher.length; i++) {
-                if (typeof matcher[i] !== "function") {
-                    throw "Prerequisite matchers must be functions";
-                }
-            }
-            this._matcher = matcher;
+    constructor(predicate?:Function) {
+        if (predicate) {
+            this._predicate = predicate;
         }
     }
 }
@@ -42,6 +30,6 @@ export class Prerequisite<T> {
  * Marks a type that can have prerequisites.
  */
 export interface HasPrerequisites<T> {
-    prerequisites:Array<Prerequisite<T>>;
+    prerequisites:Prerequisite<T>;
     meetsPrerequisites(target:T):boolean;
 }
