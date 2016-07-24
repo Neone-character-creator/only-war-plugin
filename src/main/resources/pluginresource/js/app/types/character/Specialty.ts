ca482a5bea952bf008b4f0cc9c49390c713ee1ad
Damien <damienmarble@gmail.com>
2016-07-24 01:19:31 -0700

    Added a type to CharacterModifier indicating if it is a Guardsman or Specialist type.
    Added some missing getters and implemented the apply and unapply methods.

--- src/main/resources/pluginresource/js/app/types/character/Specialty.ts
+++ src/main/resources/pluginresource/js/app/types/character/Specialty.ts
@@ -4,16 +4,58 @@ import {Trait} from "./Trait";
 import {Item} from "./items/Item";
 import {Characteristic} from "./Characteristic";
 import {OnlyWarCharacter} from "./Character";
-import {Skill} from "./Skill";
+import {Skill, SkillDescription} from "./Skill";
 /**
  * Created by Damien on 6/29/2016.
  */
 export class Specialty extends CharacterModifier {
-    constructor(characteristics:Map<Characteristic, number>, skills:Array<Skill>, talents:Array<Talent>, aptitudes:Array<string>, traits:Array<Trait>, kit:Map<Item, number>, wounds:number, optionalModifiers:Array<SelectableModifier>) {
-        super(characteristics, skills, talents, aptitudes, traits, kit, wounds, 0, OnlyWarCharacterModifierTypes.SPECIALTY);
+    private _name:string;
+    private _optionalModifiers:Array<SelectableModifier>;
+    private _specialtyType:SpecialtyType;
+
+    constructor(name:string, characteristics:Map<Characteristic, number>,
+                specialtyType:SpecialtyType,
+                skills:Map<SkillDescription, number>,
+                talents:Array<Talent>,
+                aptitudes:Array<string>,
+                traits:Array<Trait>,
+                kit:Map<Item, number>,
+                wounds:number,
+                optionalModifiers:Array<SelectableModifier>) {
+        super(characteristics, skills, talents, aptitudes, traits, kit, wounds, 0, OnlyWarCharacterModifierTypes.SPECIALTY)
+        this._specialtyType = specialtyType;
+        this._name = name;
+        this._optionalModifiers = optionalModifiers;
     }
 
-    protected applyWoundsModifier(character:OnlyWarCharacter) {
+    public apply(character:OnlyWarCharacter) {
+        super.apply(character);
         character.wounds.specialtyModifier = this.wounds;
+        for (var entry of this.characteristics.entries()) {
+            character.characteristics.get(entry[0]).specialtyModifier = entry[1];
+        }
+        ;
     }
+
+    public unapply() {
+        this._appliedTo.wounds.specialtyModifier = 0;
+        super.unapply();
+    };
+
+    get name():string {
+        return this._name;
+    }
+
+    get optionalModifiers():Array<SelectableModifier> {
+        return this._optionalModifiers;
+    }
+
+    get specialtyType():SpecialtyType {
+        return this._specialtyType;
+    }
+}
+
+export enum SpecialtyType{
+    Guardsman,
+    Specialist
 }
