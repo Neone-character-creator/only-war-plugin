0e530486c18f09f577f25c67068d59eb58747f8a
Damien <damienmarble@gmail.com>
2016-07-28 00:02:46 -0700

    Regiment provider service now uses placeholder service for skills and uses SelectableModifier instances for the optional modifiers.

--- src/main/resources/pluginresource/js/app/services/regiments.ts
+++ src/main/resources/pluginresource/js/app/services/regiments.ts
@@ -1,11 +1,9 @@
-///<reference path="../../../../../../../typings/globals/angular/index.d.ts" />
-///<reference path="../../../../../../../typings/globals/angular-resource/index.d.ts" />
 import {Characteristic} from "../types/character/Characteristic";
-import {Skill} from "../types/character/Skill";
+import {SkillDescription} from "../types/character/Skill";
 import {Talent} from "../types/character/Talent";
 import {Regiment} from "../types/character/Regiment";
 import {Trait} from "../types/character/Trait";
-import {Item, Craftsmanship} from "../types/character/items/Item";
+import {Item} from "../types/character/items/Item";
 import {SelectableModifier} from "../types/character/CharacterModifier";
 import {PlaceholderReplacement} from "./PlaceholderReplacement";
 /**
@@ -15,7 +13,7 @@ export class RegimentService {
     private _regiments;
     private _angular;
 
-    constructor($resource, $q, placeholders:PlaceholderReplacement) {
+    constructor($resource, $q, characteroptions, placeholders:PlaceholderReplacement) {
         this._regiments = $q.all({
             regiments: $resource("pluginresource/Regiment/Regiments.json").query().$promise,
             placeholders: placeholders
@@ -26,10 +24,10 @@ export class RegimentService {
                     characteristics.set(Characteristic.characteristics.get(characteristicName), regiment['fixed modifiers'].characteristics[characteristicName]);
                 }
                 var aptitudes = regiment['fixed modifiers'].aptitudes;
-                var characterSkills = new Array<Skill>();
-                for (var skillName in regiment['fixed modifiers'].skills) {
-                    var skill:Skill = result.placeholders.replace(skillName, "skill");
-                    characterSkills.push(skill);
+                var characterSkills = new Map<SkillDescription, number>();
+                var skillContainer = regiment['fixed modifiers'].skills;
+                for (var skillName in skillContainer) {
+                    characterSkills.set(result.placeholders.replace(skillName, "skill"), skillContainer[skillName]);
                 }
 
                 var characterTalents = new Array<Talent>();
@@ -57,10 +55,28 @@ export class RegimentService {
                     kit.set(item, itemDescription.count);
                 }
                 var wounds:number = regiment['fixed modifiers'].wounds;
+                var favoredWeapons = [];
+                for (var favoredWeapon of regiment['fixed modifiers']['favored weapons']) {
+                    favoredWeapons.push(result.placeholders.replace({item: {name: favoredWeapon}}, "item"));
+                }
+                var specialAbilities = []
                 var optionalModifiers = Array<SelectableModifier>();
+                if (regiment['optional modifiers']) {
+                    for (var optional of regiment['optional modifiers']) {
+                        optionalModifiers.push(new SelectableModifier(optional.selections, optional.options.map(optionGroup=> {
+                            optionGroup.description = optionGroup.map(o=>o.value).join(" or ");
+                            return optionGroup.map(option=> {
+                                option.value = result.placeholders.replace(option.value, option.property);
+                                var options = [];
+                                return option;
+                            });
+
+                        })));
+                    }
+                }
                 return new Regiment(regiment.name, characteristics, characterSkills, characterTalents, regiment['fixed modifiers'].aptitudes,
-                    characterTraits, kit, wounds, optionalModifiers);
-            })
+                    characterTraits, kit, wounds, optionalModifiers, favoredWeapons, specialAbilities);
+            });
         });
     }
 
