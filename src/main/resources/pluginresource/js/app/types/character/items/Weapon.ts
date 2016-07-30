8071e2bf2e2a903c989b0b8e5e47a6f9036473a6
Damien <damienmarble@gmail.com>
2016-07-30 03:08:05 -0700

    Added custom constructor to Weapon.

--- src/main/resources/pluginresource/js/app/types/character/items/Weapon.ts
+++ src/main/resources/pluginresource/js/app/types/character/items/Weapon.ts
@@ -1,4 +1,4 @@
-import {Item} from "./Item";
+import {Item, ItemType, Availability, Craftsmanship} from "./Item";
 /**
  * Created by Damien on 7/9/2016.
  */
@@ -7,10 +7,23 @@ export class Weapon extends Item {
     private _range:string;
     private _rateOfFire:string;
     private _damage:string;
-    private _penetration:number;
-    private _clip:number;
+    private _penetration:string;
+    private _clip:string;
     private _reload:string;
-    private _special:Array<String>;
+    private _special:Array<string>;
+
+    constructor(name:string, availability:Availability, weaponClass:string, range:string, rateOfFire:string, damage:string,
+                penetration:string, clip:string, reload:string, special:Array<string>, weight?:number, craftsmanship?:Craftsmanship) {
+        super(name, ItemType.Weapon, availability, weight, craftsmanship);
+        this._class = weaponClass;
+        this._range = range;
+        this._rateOfFire = rateOfFire;
+        this._damage = damage;
+        this._penetration = penetration;
+        this._clip = clip;
+        this._reload = reload;
+        this._special = special;
+    }
 
     get class():string {
         return this._class;
@@ -28,11 +41,11 @@ export class Weapon extends Item {
         return this._damage;
     }
 
-    get penetration():number {
+    get penetration():string {
         return this._penetration;
     }
 
-    get clip():number {
+    get clip():string {
         return this._clip;
     }
 
