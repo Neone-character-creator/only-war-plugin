0826c335c4005431389f5bd77f748d6f3923cee0
Damien <damienmarble@gmail.com>
2016-07-30 02:52:46 -0700

    Added logic for replacing traits and characteristics.

--- src/main/resources/pluginresource/js/app/services/PlaceholderReplacement.ts
+++ src/main/resources/pluginresource/js/app/services/PlaceholderReplacement.ts
@@ -1,6 +1,7 @@
 import {Talent} from "../types/character/Talent";
 import {Item, ItemType, Availability} from "../types/character/items/Item";
 import {CharacterOptionsService} from "./CharacterOptionsService";
+import {Characteristic} from "../types/character/Characteristic";
 /**
  * Created by Damien on 7/15/2016.
  */
@@ -21,6 +22,7 @@ export class PlaceholderReplacement {
     public replace(placeholder:any, type:string):any {
         switch (type) {
             case "talent":
+            {
                 var talentName = placeholder.substring(0, placeholder.indexOf("(") == -1 ? placeholder.length : placeholder.indexOf("(")).trim();
                 var specialization = placeholder.substring(placeholder.indexOf("(") + 1, placeholder.indexOf(")"));
                 var talent:Talent = this._characteroptions.talents.find(talent=> {
@@ -30,31 +32,37 @@ export class PlaceholderReplacement {
                     throw "Tried to find a replacement for talent " + placeholder + " but failed.";
                 }
                 return new Talent(talent.name, talent.source, talent.tier, talent.aptitudes, specialization, talent.prerequisites);
+            }
             case "item":
+            {
                 var item = this._characteroptions.items.find(item=> {
-                    return item.name === placeholder.item.name;
+                    return item.name === placeholder.name;
                 });
                 if (!item) {
                     item = this._characteroptions.weapons.find(weapon=> {
-                        return weapon.name === placeholder.item.name;
+                        return weapon.name === placeholder.name;
                     })
                 }
                 if (!item) {
                     item = this._characteroptions.armor.find(armor=> {
-                        return armor.name === placeholder.item.name;
+                        return armor.name === placeholder.name;
                     })
                 }
                 if (!item) {
                     item = this._characteroptions.vehicles.find(vehicle=> {
-                        return vehicle.name === placeholder.item.name;
+                        return vehicle.name === placeholder.name;
                     })
                 }
                 if (!item) {
-                    console.log("Tried to find item for " + placeholder.item.name + " but failed.");
-                    item = new Item(placeholder.item.name, ItemType.Other, Availability.Common);
+                    console.log("Tried to find item for " + placeholder.name + " but failed.");
+                    item = new Item(placeholder.name, ItemType.Other, Availability.Common);
+                    item['main weapon'] = placeholder['main weapon'];
+                    item['armor'] = placeholder['armor'];
                 }
+            }
                 return item;
             case "skill":
+            {
                 var skillName = placeholder.indexOf("(") === -1 ? placeholder : placeholder.substring(0, placeholder.indexOf("(")).trim();
                 var skill = this._characteroptions.skills.find(function (skill) {
                     return skill.name === skillName;
@@ -64,6 +72,22 @@ export class PlaceholderReplacement {
                     placeholder.indexOf("(") === -1 ? placeholder : placeholder.substring(placeholder.indexOf("(") + 1, placeholder.indexOf(")")).trim();
                 }
                 return skill;
+            }
+            case "characteristic":
+            {
+                return Characteristic.characteristics.get(placeholder);
+            }
+            case "trait":
+            {
+                var traitName = placeholder.indexOf("(") === -1 ? placeholder : placeholder.substring(0, placeholder.indexOf("(")).trim();
+                var rating = placeholder.indexOf("(") === -1 ? null : Number.parseInt(placeholder.substring(placeholder.indexOf("(") + 1, placeholder.indexOf(")")));
+                var foundTrait = this._characteroptions.traits.find((trait)=> {
+                    return trait.name === traitName;
+                });
+                foundTrait = angular.copy(foundTrait);
+                foundTrait.rating = rating;
+                return foundTrait;
+            }
             default:
                 throw "Incorrect type name, must be skill, talent, item or power.";
         }
