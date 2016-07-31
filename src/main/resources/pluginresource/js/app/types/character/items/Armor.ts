d6a55dcb9553bc80611becd3909d1710165b5d2b
Damien <damienmarble@gmail.com>
2016-07-30 23:37:38 -0700

    Added an armor type property to Armor.

--- src/main/resources/pluginresource/js/app/types/character/items/Armor.ts
+++ src/main/resources/pluginresource/js/app/types/character/items/Armor.ts
@@ -3,12 +3,26 @@ import {Item, ItemType, Availability, Craftsmanship} from "./Item";
  * Created by Damien on 7/9/2016.
  */
 export class Armor extends Item {
-    private locations:Array<string>;
-    private ap:number;
+    private _locations:Array<string>;
+    private _ap:number;
+    private _armorType:string;
 
-    constructor(name:String, availability:Availability, locations:Array<string>, ap:number, weight?:Number, craftsmanship?:Craftsmanship) {
+    constructor(name:String, availability:Availability, locations:Array<string>, ap:number, armorType:string, weight?:Number, craftsmanship?:Craftsmanship) {
         super(name, ItemType.Armor, availability, weight, craftsmanship);
-        this.locations = locations;
-        this.ap = ap;
+        this._locations = locations;
+        this._ap = ap;
+        this._armorType = armorType;
+    }
+
+    get locations():Array<string> {
+        return this._locations;
+    }
+
+    get ap():number {
+        return this._ap;
+    }
+
+    get armorType():string {
+        return this._armorType;
     }
 }
